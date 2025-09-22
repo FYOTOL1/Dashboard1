01cba6d25060fcf6557c75075ba7030d5d56e77c
Ahmed Akl <fyotol@example.com>
2025-09-22 04:29:48 +0300

    test responsive

--- src/components/main/SideBar.tsx
+++ src/components/main/SideBar.tsx
@@ -2,11 +2,11 @@ import SideCard from "./SideCard"
 
 const SideBar = () => {
     return (
-        <div className="hidden md:flex items-center flex-col md:w-4/20 min-h-[calc(100vh-64px)] gap-4 py-6 ps-0 pe-3">
-            <SideCard data={[1, 4, 2, 5]}  borderColor="blue" />
-            <SideCard data={[1, 4, 2, 5]}  borderColor="blue" />
-            <SideCard data={[1, 4, 2, 5]}  borderColor="blue" />
-            <SideCard data={[1, 4, 2, 5]}  borderColor="blue" />
+        <div className="hidden md:flex items-center flex-col md:w-6/20 min-h-[calc(100vh-64px)] gap-4 py-6 ps-0 pe-3">
+            <SideCard data={[1, 4, 2, 5]} borderColor="blue" />
+            <SideCard data={[1, 4, 2, 5]} borderColor="blue" />
+            <SideCard data={[1, 4, 2, 5]} borderColor="blue" />
+            <SideCard data={[1, 4, 2, 5]} borderColor="blue" />
         </div>
     )
 }
--- src/components/main/SideCard.tsx
+++ src/components/main/SideCard.tsx
@@ -4,11 +4,11 @@ const SideCard = ({ data, borderColor }: { data: number[], borderColor: string }
     return (
         <div className="flex flex-col justify-between w-full bg-white gap-3 rounded min-h-32">
             <div className="flex flex-col-reverse lg:flex-row gap-3 w-full p-4">
-                <div className="flex flex-col min-w-fit gap-3 text-xs font-bold">
+                <div className="flex flex-col min-w-1/3 gap-3 text-xs font-bold">
                     <p className="text-[10px] text-gray-700">Total Sales</p>
                     <p className="text-zinc-600">$290.90</p>
                 </div>
-                <div className="w-full outline outline-gray-200">
+                <div className="w-full max-w-2/3 outline outline-gray-200">
                     <CleanCharts data1={data} borderColor1={borderColor} data2={[1, 2, 1, 1]} borderColor2="gray" />
                 </div>
             </div>
